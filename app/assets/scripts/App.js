95c9a793e39e82092a716fb1a2e5139bc8468227
Miguel <migherdcas@gmail.com>
2018-07-17 20:48:11 +0200

    Small problems fixed.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,5 @@
 import ApiCall from './modules/ApiCall';
 import Test from './modules/Test';
-import API from './API';
 
 var apiCall = new ApiCall();
 apiCall.call();
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ var es2015 = require('babel-preset-es2015');
 module.exports = {
     entry: {
         App: "./app/assets/scripts/App.js",
-        Vendor: "./app/assets/scripts/Vendor.js",
-        API: "./app/assets/scripts/API.js"
+        Vendor: "./app/assets/scripts/Vendor.js"
     },
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
